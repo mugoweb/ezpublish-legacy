5ad1b6e8210c4e947148e55451cc7eb0bf898f1d
Philipp Kamps <pkamps@mugo.ca>
2017-01-11 07:39:35 -0500

    more complete logic to target specific php version and the var_dump differences

--- tests/tests/kernel/private/rest/ezprest_request_parser_regression.php
+++ tests/tests/kernel/private/rest/ezprest_request_parser_regression.php
@@ -85,8 +85,8 @@ class ezpRestHttpRequestParserRegression extends ezpRegressionTest
         $var = var_export( $var, true );
         // 50505 is PHP 5.5.5
         // 50509 is PHP 5.5.9
-        // version from 50505 to 50509 are also affected
-        if ( PHP_VERSION_ID <= 50430 || ( PHP_VERSION_ID >= 50505 && PHP_VERSION_ID <= 50509 ) )
+        // version from 50500 to 50513 are also affected
+        if ( PHP_VERSION_ID < 50430 || ( PHP_VERSION_ID >= 50500 && PHP_VERSION_ID < 50514 ) )
         {
             $var = preg_replace( '%(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})%', '$1.000000', $var );
         }
