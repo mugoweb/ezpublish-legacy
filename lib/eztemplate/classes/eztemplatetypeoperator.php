e828c54709a8ed4916f43042d6d1b01c1b8c89b8
Holger Marx <holger.marx@mediata.net>
2023-08-08 12:15:53 +0200

    Consideration of changed error handling of PHP get_class function
    Since PHP 8.0.0:
    Calling get_class function from outside a class, without any arguments, will now throw an Error. Previously, an E_WARNING was raised and the function returned false.

--- lib/eztemplate/classes/eztemplatetypeoperator.php
+++ lib/eztemplate/classes/eztemplatetypeoperator.php
@@ -263,7 +263,7 @@ class eZTemplateTypeOperator
 
             case $this->IsClassName:
             {
-                $code .= '( strtolower( get_class( %1% ) ) == strtolower( %2% ) );';
+                $code .= '( strtolower( is_object( %1% ) ? get_class( %1% ) : "" ) == strtolower( %2% ) );';
                 $values[] = $parameters[1];
             } break;
 
@@ -289,7 +289,7 @@ class eZTemplateTypeOperator
 
             case $this->GetClassName:
             {
-                $code .= 'strtolower( get_class( %1% ) );';
+                $code .= 'strtolower( is_object( %1% ) ? get_class( %1% ) : "" );';
             } break;
         }
 
